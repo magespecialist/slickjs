ed513b4dd735d56296b22821bf292480b22cbc8e
Lorenzo Stramaccia <lorenzo.stramaccia@magespecialist.it>
2017-10-18 17:29:41 +0200

    initial commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,30 @@
+/**
+ * MageSpecialist
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@magespecialist.it so we can send you a copy immediately.
+ *
+ * @category   MSP
+ * @package    Slick
+ * @copyright  Copyright (c) 2017 Skeeller srl (http://www.magespecialist.it)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+var config = {
+    'map': {
+        '*': {
+            'slick': 'MSP_Slick/js/slickjs',
+            'slick-min': 'MSP_Slick/js/slick.min'
+        }
+    },
+    'shim': {
+        'MSP_Slick/js/slick.min': ['jquery']
+    }
+};
